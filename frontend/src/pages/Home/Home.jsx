0415db53acf36a3954e8f7f83cc3f7009fed4a20
Sumit De <sumitde24012004@gmail.com>
2024-08-29 20:34:50 +0530

    home page frontend done

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../components/FoodDisaplay/FoodDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
 
 const Home = () => {
 
@@ -11,6 +13,8 @@ const Home = () => {
     <div>
         <Header />
         <ExploreMenu category={category} setCategory={setCategory}/>
+        <FoodDisplay category={category}/>
+        <AppDownload />
     </div>
   )
 }

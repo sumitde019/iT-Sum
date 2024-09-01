6ab0ed3ffcd870588330da2201877d848416e441
Sumit De <sumitde24012004@gmail.com>
2024-09-02 01:12:15 +0530

    Bug fixed in Cart page and order page is added

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react'
 import './Cart.css'
 import {StoreContext} from '../../context/StoreContext'
+import {useNavigate} from 'react-router-dom'
 
 const Cart = () => {
 
   const {cartItems,food_list,removeFromCart,getTotalCartAmount} = useContext(StoreContext)
+
+  const navigate = useNavigate()
 
   return (
     <div className='cart'>
@@ -48,15 +51,15 @@ const Cart = () => {
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>Rs{2}</p>
+              <p>Rs{getTotalCartAmount()===0?0:15}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <b>Total</b>
-              <b>Rs{getTotalCartAmount()+2}</b>
+              <b>Rs{getTotalCartAmount()===0?0:getTotalCartAmount()+15}</b>
             </div>
           </div>
-          <button>PROCEED TO CHECKOUT</button>
+          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
           <div>

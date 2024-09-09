8ca5ed841366463e343136092b7a9f73fa0f362a
Sumit <116253609+sumitde019@users.noreply.github.com>
2024-09-09 21:53:40 +0530

    db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,3 @@ import mongoose from "mongoose";
 export const connectDB = async() => {
     await mongoose.connect('mongodb+srv://name:password@cluster0.wakmgrx.mongodb.net/food_del').then(()=>console.log("DB connected"));
 }
-
-
-//mongodb+srv://Sumit:sumit24de@cluster0.wakmgrx.mongodb.net/food_del?retryWrites=true&w=majority&appName=Cluster0

7fe2d0aceda15e420ce957e668544a230344ea8a
Sumit De <sumitde24012004@gmail.com>
2024-09-10 12:26:38 +0530

    bug fixed

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async() => {
-    await mongoose.connect('mongodb+srv://name:password@cluster0.wakmgrx.mongodb.net/food_del').then(()=>console.log("DB connected"));
-}

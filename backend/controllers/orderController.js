fed621b2e5e3b6d9c3a52cfb1caf041e9ee4732e
Sumit De <sumitde24012004@gmail.com>
2024-09-29 00:03:40 +0530

    ➕ Order page added

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -70,4 +70,32 @@ const placeOrder = async (req,res) => {
     }
 }
 
-export {placeOrder}
+const verifyOrder = async (req,res) => {
+    const {orderId,success} = req.body;
+    try {
+        if(success=="true"){
+            await orderModel.findByIdAndUpdate(orderId,{payment:true});
+            res.json({success:true,message:"Paid"})
+        }
+        else{
+            await orderModel.findByIdAndDelete(orderId);
+            res.json({success:false,message:"Not Paid"})
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:"Error"})
+    }
+}
+
+// users order for frontend
+const userOrders = async (req,res) => {
+    try {
+        const orders = await orderModel.find({userId:req.body.userId});
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+export {placeOrder, verifyOrder, userOrders}
